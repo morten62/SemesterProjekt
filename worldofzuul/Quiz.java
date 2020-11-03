250bafe7cf86dc397d24cb2ea64c76cdce860294
MrKetuz <meldgaardsen@live.dk>
2020-11-03 13:53:05 +0100

    Opdateret quiz klassen

--- worldofzuul/Quiz.java
+++ worldofzuul/Quiz.java
@@ -1,8 +1,22 @@
 package worldofzuul;
-
+import java.io.*;
 public class Quiz {
-
-    private String getAnswer(String s){
+    private void createFile(){
+        try{
+            File obj = new File("Answers.txt");
+            }
+        catch(Exception e) {
+            System.out.println("Der er opstået en fejl");
+            e.printStackTrace();
+        }
+    }
+    private void startQuiz(){
+        boolean b = false;
+        int i = 1;
+        System.out.println("Dette er en quiz som tester din viden om dette emne.");
+        System.out.println("Denne quiz fremkommer også igen efter spillet er slut.");
+    }
+    private void getAnswer(String s){
         int i;
         boolean b = false;
         while(b=false) {
