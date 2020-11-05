7b95095d919b9f76e67b5a1fde8eb97c048229a6
simon <simon@spedsbjerg.dk>
2020-11-05 10:03:12 +0100

    lille rettelse i Inventory
    # Conflicts:
    #	worldofzuul/Inventory.java

--- worldofzuul/Inventory.java
+++ worldofzuul/Inventory.java
@@ -2,7 +2,7 @@ package worldofzuul;
 import java.util.ArrayList;
 
 public class Inventory {
-    public static ArrayList<Items> inventory = new ArrayList<Items>();
+    public ArrayList<Items> inventory = new ArrayList<Items>();
 
     //add item and it will be placed last in the list
     public void addToInventory(Items item){
@@ -10,46 +10,40 @@ public class Inventory {
     }
 
     //takes a string and removes the first from list where the name fits the input
-    public Items removeFromInventory(String name){
+    public void removeFromInventory(String name){
         if (!(inventory.size() == 0)) {
             for (int i = 0; inventory.size() < i; i++){
                 if (inventory.get(i).name == name){
-                    Items items = inventory.get(i);
                     inventory.remove(i);
-                    return items;
+                    break;
                 }
             }
         }
         else {
             System.out.println("\nDer er ingen ting i din inventar!\n");
         }
-        return null;
     }
 
     //removes the first item which is identical
-    public Items removeFromInventory(Items item){
+    public void removeFromInventory(Items item){
         if (!(inventory.size() == 0)) {
             for (int i = 0; inventory.size() < i; i++){
                 if (inventory.get(i) == item){
-                    Items items = inventory.get(i);
                     inventory.remove(i);
-                    return items;
+                    break;
                 }
             }
         }
         else {
             System.out.println("\nDer er ingen ting i din inventar!");
         }
-        return null;
     }
 
     //removes item by index
-    public Items removeFromInventory(int item){
+    public void removeFromInventory(int item){
         if (!(inventory.size() == 0)){
             try {
-                Items items = inventory.get(item);
                 inventory.remove(item);
-                return items;
             }
             catch (IndexOutOfBoundsException e){
                 System.out.println("\nDer er ingen genstand som matcher det tilspurgte index! :: " + e);
@@ -58,7 +52,6 @@ public class Inventory {
         else {
             System.out.println("\nDer er ingen ting i din inventar!");
         }
-        return null;
     }
 
     //print inventory
