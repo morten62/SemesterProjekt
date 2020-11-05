adda2c21c8881908474cf001525ffed25d8bba32
miketoftum <mitof20@student.sdu.dk>
2020-11-05 09:53:35 +0100

    Items and Article class uncomplete

---
+++ worldofzuul/Article.java
@@ -0,0 +1,20 @@
+package worldofzuul;
+
+public class Article extends Items {
+
+    public Article(String name, String description) {
+
+        super(name, description);
+    }
+
+
+    Article wellArticle = new Article("Artikel om brønde",
+            "Selve artiklen. Graves. Åben/Lukket. Pumper");
+    Article liningArticle = new Article("Artikel om foring",
+            "Selve artiklen. Omhandler de tre typer af foring, og deres fordele og ulepmer");
+    Article pumpArticle = new Article("Artikel om pumper",
+            "Selve artiklen. Omhandler de forskellige pumper.");
+    Article wellPlacement = new Article("working title Artikel om brøndsteder",
+            "Omhandler hvilke steder det er godt at placere brønde.")
+
+}
