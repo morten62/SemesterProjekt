6fc56070fd791725dcd1e39d4008b1670107a84e
Rasmus <hvalrosstian@gmail.com>
2020-11-08 18:36:18 +0100

    Tilføjede spørgsmål og begrænset svar til tal mellem 1 og 5.

--- worldofzuul/Quiz.java
+++ worldofzuul/Quiz.java
@@ -33,7 +33,7 @@ public class Quiz {
     // Starter quizzen ved at bestemme om det er før eller efter spillet er færdigt og indsætter resultatet i Answers.txt
     // Quizzen vil så gå igennem en for loop med alle spørgsmålene som er i det String array som bliver defineret her også.
     public void startQuiz(){
-        String[] questions = {"Question1", "Question2", "Question3", "Question4", "Question5"};
+        String[] questions = {"Vil du mene at din viden af indvinding af rent drikkevand er høj?", "Vil du sige at alle mennesker i verden har lige god mulighed for rent drikkevand?", "Vil du sige at spillet har været med til at forhøje din viden om indvinding af rent drikkevand og dets udfordringer?", "Vil du sige at spillet fremvist at der er nogle steder hvor det ikke er muligt at lave en ordentlig løsning pga. ressource mangler?", "Var spillet sjovt?"};
         if (number==1){
             try {
                 writer.write("Svar før spillet:");
@@ -42,6 +42,7 @@ public class Quiz {
                 e.printStackTrace();
                 System.out.println("Der er opstået en fejl i udskrivning af tegn");
             }
+
         }
         else if (number==2){
             try {
@@ -52,18 +53,33 @@ public class Quiz {
                 System.out.println("Der er opstået en fejl i udskrivning af tegn");
             }
         }
-        number++;
+
         int temp = 1;
         System.out.println("Dette er en quiz som tester din viden om dette emne.");
-        System.out.println("Denne quiz fremkommer også igen efter spillet er slut.");
-        for(int v = 0; v<5;v++){
-             System.out.println("Spørgsmål "+temp+": "+questions[v]);
-             getAnswer(v);
-            temp++;
+        System.out.println("Denne quiz fremkommer også igen efter spillet er slut, dog med andre spørgsmål.");
+        System.out.println("Du har følgende svarmulligheder:");
+        System.out.println("1 - Meget uenig");
+        System.out.println("2 - Lidt uenig");
+        System.out.println("3 - Hverken uenig eller enig/Ved ikke");
+        System.out.println("4 - Lidt enig");
+        System.out.println("5 - Meget enig");
+        if(number==1) {
+            for (int v = 0; v < 2; v++) {
+                System.out.println("Spørgsmål " + temp + ": " + questions[v]);
+                getAnswer(v);
+                temp++;
+            }
         }
-
+        if(number==2){
+            for (int v = 2; v < 5; v++) {
+                System.out.println("Spørgsmål " + temp + ": " + questions[v]);
+                getAnswer(v);
+                temp++;
+            }
+        }
+        number++;
     }
-    //Indsamler værdien v som beskriver hvad for et spørgsmål det er. Tager næste String som brugerinput og prøver at konverter til int
+    //Indsamler værdien v som beskriver hvad for et spørgsmål det er. Tager næste String som brugerinput og prøver at konverter til int mellem 1 og 5.
     //Hvis det fejler køres brugeren igennem den samme process indtil et validt string som kan konverteres til int findes.
     private void getAnswer(int v){
         int i;
@@ -72,8 +88,13 @@ public class Quiz {
             String s = sc.nextLine();
             try {
                 i = Integer.parseInt(s);
-                assembleAnswer(i, v);
-                bool = true;
+                if(i>=1 && i<=5) {
+                    assembleAnswer(i, v);
+                    bool = true;
+                }
+                else{
+                    System.out.println("Du skal svare med et nummer mellem 1 og 5");
+                }
             } catch (Exception e) {
                 System.out.println("Ikke et validt tal, prøv igen.");
             }
